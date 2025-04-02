356a73b06ad272f3e6e1d3bf389c17218dc8773e
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-04-02 19:10:35 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+# set page configs
+st.set_page_config(
+    page_title="PCA Analytics",
+    layout="wide",)
+
+# title
+st.title('PCA Statistics Analysis :church:', help = 'All data is from [The PCA](https://presbyteryportal.pcanet.org/Report/StatsReport)')
