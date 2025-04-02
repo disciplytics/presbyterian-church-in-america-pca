79cb414021af6d1a42c0782f4eb0c266647842eb
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-04-02 19:15:00 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import streamlit as st
 # set page configs
 st.set_page_config(
     page_title="PCA Analytics",
-    layout="wide",)
+    layout="wide",
+    page_icon = 'https://jimdo-storage.freetls.fastly.net/image/446612637/7c401e7a-6b6d-4ec8-84a5-4ab2cae82c9e.png?quality=80,90&auto=webp&disable=upscale&width=1024&height=576&trim=0,0,0,0',
+)
 
 # title
 st.title('PCA Statistics Analysis :church:', help = 'All data is from [The PCA](https://presbyteryportal.pcanet.org/Report/StatsReport)')
