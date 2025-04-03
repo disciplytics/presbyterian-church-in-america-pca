8c94e0fc532d7746b51c5a5b9fc0351c4ae4e611
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-04-02 20:11:06 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,23 @@ import streamlit as st
 
 home_page = st.Page(
   'pages/home.py', 
-  title = 'home', 
+  title = 'Home', 
   icon=":material/home:", 
   default=True
+)
+
+report_2024_page = st.Page(
+  'pages/report_2024.py', 
+  title = 'Technical Reports', 
+  icon=":material/lab_profile:", 
+  default=False
 )
 
 # navigation 
 pg = st.navigation(
         {
             "": [home_page],
+            "Technical Reports": [report_2024],
           
         }
     )
