ac71d2acf64f3285cbdc42cfdfea2e7b63f7af78
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-04-02 20:32:06 -0400

    Create dash_reports.py

---
+++ pages/dash_reports.py
@@ -0,0 +1,15 @@
+import streamlit as st
+# set page configs
+st.set_page_config(
+    page_title="PCA Dashboards",
+    layout="wide",
+    page_icon = 'https://jimdo-storage.freetls.fastly.net/image/446612637/7c401e7a-6b6d-4ec8-84a5-4ab2cae82c9e.png?quality=80,90&auto=webp&disable=upscale&width=1024&height=576&trim=0,0,0,0',
+)
+
+# LOGO
+st.image("https://media.licdn.com/dms/image/v2/D4E16AQGCrog5mV8nBQ/profile-displaybackgroundimage-shrink_350_1400/B4EZUAA8ZzHgAY-/0/1739462002589?e=1744848000&v=beta&t=miQyzZN82YjcYs9B_Mc-UVhaKt01dqVnPE56CnaVPbw",
+        width = 250)
+
+# title
+st.title('PCA Dashboards :church:')
+st.caption('Reports are maintained by [Disciplytics, LLC](https://www.disciplytics.com/)')
