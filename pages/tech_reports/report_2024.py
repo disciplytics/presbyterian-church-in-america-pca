516a4b9a13a2ca4261078a565644eaf00fc2a6f2
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-04-02 20:12:18 -0400

    Update and rename pages/report_2024.py to pages/tech_reports/report_2024.py

---
+++ pages/tech_reports/report_2024.py
@@ -0,0 +1,27 @@
+from streamlit import markdown
+markdown('''    
+        ##### Title page
+        
+        ##### Acknowledgements
+        
+        ##### Summary
+        
+        ##### Table of Contents
+        
+        ##### Introduction/Terms of Reference/Scope
+        
+        ##### Procedure
+        
+        ##### Findings
+        
+        ##### Conclusions
+        
+        ##### Recommendations
+        
+        ##### References/Bibliography
+        
+        ##### Appendices
+    
+    
+        '''
+    )
