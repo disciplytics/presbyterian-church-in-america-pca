0710dfc5da9c6873dbc35081c30cf888aa70c245
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-04-09 19:54:09 -0400

    Update dashboards.py

--- pages/dashboards.py
+++ pages/dashboards.py
@@ -35,9 +35,8 @@ state_sel = st.multiselect('Select a State', state_options)
 st.write(state_sel)
 
 city_sel = None
-church_sel = None
 
-if state_sel:
+if len(state_sel) > 0:
     city_sel = st.multiselect('Select a City', city_options)
     st.write(city_sel)
     
