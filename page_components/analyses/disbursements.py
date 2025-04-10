8539593983bc7f1a3639284d7fd7c691510bd2b0
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-04-09 21:23:10 -0400

    Create disbursements.py

---
+++ page_components/analyses/disbursements.py
@@ -0,0 +1,30 @@
+def disbursements(df):
+  import streamlit as st
+  import pandas as pd
+  
+  df = df.rename(columns = {
+          'YEAR': 'Year',
+          'ADULTBAPT': 'AdultBapt', 'INFANTBAPT': 'InfantBapt', 
+          'RULINGELDERS': 'RulingElders', 'DEACONS': 'Deacons', 
+          'FAMILYUNITS': 'FamilyUnits', 
+          'SUNDAYSCHOOLATTEND': 'SundaySchoolAttend', 'SMALLGROUPATTEND': 'SmallGroupAttend', 'ESTMORNATTEND': 'EstMornAttend',
+  }
+                )
+
+                            
+  bapt_df = df[['Year', 'AdultBapt', 'InfantBapt']].melt(id_vars=['Year'], var_name='Metric', value_name='Count')
+  off_df = df[['Year', 'RulingElders', 'Deacons']].melt(id_vars=['Year'], var_name='Metric', value_name='Count')
+  fam_df = df[['Year', 'FamilyUnits']].melt(id_vars=['Year'], var_name='Metric', value_name='Count')
+  attend_df = df[['Year', 'SundaySchoolAttend', 'SmallGroupAttend', 'EstMornAttend']].melt(id_vars=['Year'], var_name='Metric', value_name='Count')
+
+  st.subheader('Baptism Trends')
+  st.bar_chart(bapt_df.groupby(['Year', 'Metric'])['Count'].sum().reset_index(), x = 'Year', y = 'Count', color = 'Metric', stack = False)
+
+  st.subheader('Officer Trends')
+  st.bar_chart(off_df.groupby(['Year', 'Metric'])['Count'].sum().reset_index(), x = 'Year', y = 'Count', color = 'Metric', stack = False)
+
+  st.subheader('Family Trends')
+  st.bar_chart(fam_df.groupby(['Year', 'Metric'])['Count'].sum().reset_index(), x = 'Year', y = 'Count', color = 'Metric', stack = False)
+  
+  st.subheader('Attendance Trends')
+  st.bar_chart(attend_df.groupby(['Year', 'Metric'])['Count'].sum().reset_index(), x = 'Year', y = 'Count', color = 'Metric', stack = False)
