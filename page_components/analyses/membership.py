e0eee5d62c10925e7283133c1699b3ebdfa6fa01
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-04-09 20:42:32 -0400

    Update membership.py

--- page_components/analyses/membership.py
+++ page_components/analyses/membership.py
@@ -1,4 +1,11 @@
 def membership(df):
   import streamlit as st
+  import pandas as pd
 
-  st.bar_chart(df.groupby(['YEAR'])['COMM'].sum().reset_index(), x = 'YEAR', y = 'COMM')
+  df['Total Members'] = df['COMM'] + df['NONCOMM']
+
+  df = df.rename(columns = {'COMM': 'Comm', 'NONCOMM': 'NonComm, 'YEAR', 'Year'})
+                            
+  df = df[['Year', 'Comm', 'NonComm', 'Total Members']].melt(id_vars=['Year'], var_name='Metric', value_name='Count')
+
+  st.bar_chart(df.groupby(['Year', 'Metric'])['Count'].sum().reset_index(), x = 'YEAR', y = 'Count', color = 'Metric')
