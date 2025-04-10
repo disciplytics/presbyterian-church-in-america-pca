af204d11daeb650dadc354ffa9b9c9581e1d2249
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-04-09 20:05:08 -0400

    Update dashboards.py

--- pages/dashboards.py
+++ pages/dashboards.py
@@ -24,24 +24,39 @@ def load_stats_data():
 
 stats_df = load_stats_data()
 
+# query data
 
-# get filter options
-state_options = sort(stats_df['STATE'].fillna('No State Reported').unique())
-city_options = sort(stats_df['CITY'].fillna('No City Reported').unique())
-church_options = sort(stats_df['CHURCH'].fillna('No Church Reported').unique())
+def query_data(df):
+    df['STATE']=df['STATE'].fillna('No State Reported')
+    df['CITY']=df['CITY'].fillna('No City Reported')
+    df['CHURCH']=df['CHURCH'].fillna('No Church Reported')
 
-# conditionally show filters if selections are made
-state_sel = st.multiselect('Select a State', state_options)
-st.write(state_sel)
+    # BUILD STATE FILTER
+    # get filter options for state
+    state_options = sort(df['STATE'].unique())
+    # streamlit object for state filter
+    state_sel = st.multiselect('Select a State', state_options)
+    st.write(state_sel)
 
-city_sel = None
-
-if len(state_sel) > 0:
-    city_sel = st.multiselect('Select a City', city_options)
-    st.write(city_sel)
+    # BUILD CITY FILTER IF STATE IS FILTERED 
+    city_sel = None
     
-if city_sel:
-    church_sel = st.multiselect('Select a Church', church_options)
-    st.write(church_sel)
-    
+    if len(state_sel) > 0:
+        filtered_df = df[df['STATE'].isin(state_sel)]
+        city_options = sort(df['CITY'].unique())
+        city_sel = st.multiselect('Select a City', city_options)
+        st.write(city_sel)
+        
+    # BUILD CHURCH FILTER IF STATE IS FILTERED 
+    if city_sel:
+        filtered_df = df[(df['STATE'].isin(state_sel)) & (df['CITY'].isin(city_sel))]
+        church_options = sort(df['CHURCH'].unique())
+        church_sel = st.multiselect('Select a Church', church_options)
+        st.write(church_sel)
+
+    return filtered_df
+filtered_df = query_data(stats_df)   
+
+st.dataframe(filtered_df)
 st.dataframe(stats_df)
+
