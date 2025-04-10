50d5e204fc7b6210c96264de14427300f178754f
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-04-09 20:53:24 -0400

    Update membership.py

--- page_components/analyses/membership.py
+++ page_components/analyses/membership.py
@@ -3,9 +3,22 @@ def membership(df):
   import pandas as pd
 
   df['Total Members'] = df['COMM'] + df['NONCOMM']
+  df['Total Additions'] = df['PROFCHILD'] + df['PROFADULT'] + df['TRANSLETTER'] + df['REAFFIRMATION'] + df['RESTORED']
 
-  df = df.rename(columns = {'COMM': 'Comm', 'NONCOMM': 'NonComm', 'YEAR': 'Year'})
+  df = df.rename(columns = {
+          'YEAR': 'Year',
+          'COMM': 'Comm', 'NONCOMM': 'NonComm', 
+          'PROFCHILD': 'ProfChild', 'PROFADULT': 'ProfAdult', 'TRANSLETTER': 'TransLetter', 'REAFFIRMATION': 'Reaffirmation', 'RESTORED': 'Restored',
+  
+  }
+                )
                             
-  df = df[['Year', 'Comm', 'NonComm', 'Total Members']].melt(id_vars=['Year'], var_name='Metric', value_name='Count')
+  mems_df = df[['Year', 'Comm', 'NonComm', 'Total Members']].melt(id_vars=['Year'], var_name='Metric', value_name='Count')
+  adds_df = df[['Year', 'ProfChild', 'ProfAdult', 'TransLetter', 'Reaffirmation', 'Restored', 'Total Additions']].melt(id_vars=['Year'], var_name='Metric', value_name='Count')
 
-  st.bar_chart(df.groupby(['Year', 'Metric'])['Count'].sum().reset_index(), x = 'Year', y = 'Count', color = 'Metric', stack = False)
+  st.subheader('Total Member Trends')
+  st.bar_chart(mems_df.groupby(['Year', 'Metric'])['Count'].sum().reset_index(), x = 'Year', y = 'Count', color = 'Metric', stack = False)
+
+  st.subheader('Member Additions')
+  st.bar_chart(adds_df.groupby(['Year', 'Metric'])['Count'].sum().reset_index(), x = 'Year', y = 'Count', color = 'Metric', stack = False)
+  
