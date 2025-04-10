de7fd2c58b4342d737dc9ae6a6f90302a0be2241
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-04-09 20:14:22 -0400

    Update query_data.py

--- utils/query_data.py
+++ utils/query_data.py
@@ -9,7 +9,6 @@ def query_data(df):
     state_options = sort(df['STATE'].unique())
     # streamlit object for state filter
     state_sel = st.multiselect('Select a State', state_options)
-    st.write(state_sel)
 
     # BUILD CITY FILTER IF STATE IS FILTERED 
     city_sel = None
@@ -18,13 +17,11 @@ def query_data(df):
         filtered_df = df[df['STATE'].isin(state_sel)]
         city_options = sort(filtered_df['CITY'].unique())
         city_sel = st.multiselect('Select a City', city_options)
-        st.write(city_sel)
         
     # BUILD CHURCH FILTER IF STATE IS FILTERED 
     if city_sel:
         filtered_df = df[(df['STATE'].isin(state_sel)) & (df['CITY'].isin(city_sel))]
         church_options = sort(filtered_df['CHURCH'].unique())
         church_sel = st.multiselect('Select a Church', church_options)
-        st.write(church_sel)
 
     return filtered_df
