8dd229e27d99de9f036d026389f53c6ce89ed9d6
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-04-09 20:18:13 -0400

    Update query_data.py

--- utils/query_data.py
+++ utils/query_data.py
@@ -1,5 +1,6 @@
 def query_data(df):
     from numpy import sort
+    from streamlit import multiselect
     df['STATE']=df['STATE'].fillna('No State Reported')
     df['CITY']=df['CITY'].fillna('No City Reported')
     df['CHURCH']=df['CHURCH'].fillna('No Church Reported')
@@ -8,7 +9,7 @@ def query_data(df):
     # get filter options for state
     state_options = sort(df['STATE'].unique())
     # streamlit object for state filter
-    state_sel = st.multiselect('Select a State', state_options)
+    state_sel = multiselect('Select a State', state_options)
 
     # BUILD CITY FILTER IF STATE IS FILTERED 
     city_sel = None
@@ -16,13 +17,13 @@ def query_data(df):
     if len(state_sel) > 0:
         filtered_df = df[df['STATE'].isin(state_sel)]
         city_options = sort(filtered_df['CITY'].unique())
-        city_sel = st.multiselect('Select a City', city_options)
+        city_sel = multiselect('Select a City', city_options)
         
     # BUILD CHURCH FILTER IF STATE IS FILTERED 
     if city_sel:
         filtered_df = df[(df['STATE'].isin(state_sel)) & (df['CITY'].isin(city_sel))]
         church_options = sort(filtered_df['CHURCH'].unique())
-        church_sel = st.multiselect('Select a Church', church_options)
+        church_sel = multiselect('Select a Church', church_options)
         filtered_df = df[(df['STATE'].isin(state_sel)) & (df['CITY'].isin(city_sel)) & (df['CHURCH'].isin(church_sel))]
 
     return filtered_df
