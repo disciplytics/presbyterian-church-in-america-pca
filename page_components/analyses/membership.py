b9242a299f13a7434fe500b7ce848a918ec16c23
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-04-09 20:36:54 -0400

    Update membership.py

--- page_components/analyses/membership.py
+++ page_components/analyses/membership.py
@@ -1,4 +1,4 @@
 def membership(df):
   import streamlit as st
 
-  st.bar_chart(df.groupby(['YEAR'])['COMM'].sum().reset_index, x = 'YEAR', y = 'COMM')
+  st.bar_chart(df.groupby(['YEAR'])['COMM'].sum().reset_index(), x = 'YEAR', y = 'COMM')
