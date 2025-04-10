99c03f105288f874d228cca9a8eae23c015c804a
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-04-09 20:07:31 -0400

    Update dashboards.py

--- pages/dashboards.py
+++ pages/dashboards.py
@@ -43,14 +43,14 @@ def query_data(df):
     
     if len(state_sel) > 0:
         filtered_df = df[df['STATE'].isin(state_sel)]
-        city_options = sort(df['CITY'].unique())
+        city_options = sort(filtered_df['CITY'].unique())
         city_sel = st.multiselect('Select a City', city_options)
         st.write(city_sel)
         
     # BUILD CHURCH FILTER IF STATE IS FILTERED 
     if city_sel:
         filtered_df = df[(df['STATE'].isin(state_sel)) & (df['CITY'].isin(city_sel))]
-        church_options = sort(df['CHURCH'].unique())
+        church_options = sort(filtered_df['CHURCH'].unique())
         church_sel = st.multiselect('Select a Church', church_options)
         st.write(church_sel)
 
