fec604ca4d2617d7a8396f9f4ee6120e645da4b1
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-04-09 21:08:41 -0400

    Update general_data.py

--- page_components/analyses/general_data.py
+++ page_components/analyses/general_data.py
@@ -1,29 +1,26 @@
 def general_data(df):
   import streamlit as st
   import pandas as pd
-
-  df['Total Members'] = df['COMM'] + df['NONCOMM']
-  df['Total Additions'] = df['PROFCHILD'] + df['PROFADULT'] + df['TRANSLETTER'] + df['REAFFIRMATION'] + df['RESTORED']
-  df['Total Losses'] = df['DEATH'] + df['TRANS'] + df['REMOVEDFROMROLL'] + df['DISCIPLINE']
   
   df = df.rename(columns = {
           'YEAR': 'Year',
-          'COMM': 'Comm', 'NONCOMM': 'NonComm', 
-          'PROFCHILD': 'ProfChild', 'PROFADULT': 'ProfAdult', 'TRANSLETTER': 'TransLetter', 'REAFFIRMATION': 'Reaffirmation', 'RESTORED': 'Restored',
-          'DEATH': 'Death', 'TRANS': 'Trans', 'REMOVEDFROMROLL': 'RemovedFromRoll', 'DISCIPLINE': 'Discipline',
+          'ADULTBAPT': 'AdultBapt', 'INFANTBAPT': 'InfantBapt', 
+          'RULINGELDERS': 'RulingElders', 'DEACONS': 'Deacons', 
+          'FAMILYUNITS': 'FamilyUnits', 
+          'SUNDAYSCHOOLATTEND': 'SundaySchoolAttend', 'SMALLGROUPATTEND': 'SmallGroupAttend', 'ESTMORNATTEND': 'EstMornAttend',
   }
                 )
 
                             
-  mems_df = df[['Year', 'Comm', 'NonComm', 'Total Members']].melt(id_vars=['Year'], var_name='Metric', value_name='Count')
-  adds_df = df[['Year', 'ProfChild', 'ProfAdult', 'TransLetter', 'Reaffirmation', 'Restored']].melt(id_vars=['Year'], var_name='Metric', value_name='Count')
-  loss_df = df[['Year', 'Death', 'Trans', 'RemovedFromRoll', 'Discipline']].melt(id_vars=['Year'], var_name='Metric', value_name='Count')
+  bapt_df = df[['Year', 'AdultBapt', 'InfantBapt']].melt(id_vars=['Year'], var_name='Metric', value_name='Count')
+  off_df = df[['Year', 'RulingElders', 'Deacons', 'FamilyUnits']].melt(id_vars=['Year'], var_name='Metric', value_name='Count')
+  attend_df = df[['Year', 'SundaySchoolAttend', 'SmallGroupAttend', 'EstMornAttend']].melt(id_vars=['Year'], var_name='Metric', value_name='Count')
 
-  st.subheader('Total Member Trends')
-  st.bar_chart(mems_df.groupby(['Year', 'Metric'])['Count'].sum().reset_index(), x = 'Year', y = 'Count', color = 'Metric', stack = False)
+  st.subheader('Baptism Trends')
+  st.bar_chart(bapt_df.groupby(['Year', 'Metric'])['Count'].sum().reset_index(), x = 'Year', y = 'Count', color = 'Metric', stack = False)
 
-  st.subheader('Member Additions')
-  st.bar_chart(adds_df.groupby(['Year', 'Metric'])['Count'].sum().reset_index(), x = 'Year', y = 'Count', color = 'Metric', stack = False)
+  st.subheader('Officer Trends')
+  st.bar_chart(off_df.groupby(['Year', 'Metric'])['Count'].sum().reset_index(), x = 'Year', y = 'Count', color = 'Metric', stack = False)
 
-  st.subheader('Member Losses')
-  st.bar_chart(loss_df.groupby(['Year', 'Metric'])['Count'].sum().reset_index(), x = 'Year', y = 'Count', color = 'Metric', stack = False)
+  st.subheader('Attendance Trends')
+  st.bar_chart(attend_df.groupby(['Year', 'Metric'])['Count'].sum().reset_index(), x = 'Year', y = 'Count', color = 'Metric', stack = False)
