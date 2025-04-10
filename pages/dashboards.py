8a2b8f294406b827c86405392335d60f422a1c88
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-04-09 20:32:30 -0400

    Update dashboards.py

--- pages/dashboards.py
+++ pages/dashboards.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from page_components.utils.query_data import query_data
+from page_components.analyses.membership import membership
+
 # set page configs
 st.set_page_config(
     page_title="PCA Dashboards",
