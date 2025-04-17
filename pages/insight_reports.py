1882d427c54049121eda504adf7ddce55b06b934
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-04-17 09:33:20 -0400

    Update insight_reports.py

--- pages/insight_reports.py
+++ pages/insight_reports.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import pydeck as pdk
+from json import loads
 # set page configs
 st.set_page_config(
     page_title="Community Insight Reports",
@@ -25,3 +27,33 @@ def load_acs_data():
 acs_df = load_acs_data()
 
 st.dataframe(acs_df)
+
+
+# get geojson for selected area
+geojson_sql = "SELECT GEOJSON_VALUES FROM DISCIPLYTICS_APP.COMMUNITY_DATA.ACS_5YR_DATA WHERE GEO_NAME = " + 'Tiffin' + "LIMIT 1"
+geojson = conn.query(geojson_sql, ttl=0)
+
+# convert to dictionary
+geojson_dict = loads(geojson['GEOJSON_VALUES'][0])
+
+# create the GeoJson layer
+geojson_layer = pdk.Layer(
+    "GeoJsonLayer",
+    geojson_dict,
+    opacity=0.3,
+    stroked=False,
+    filled=True,
+    extruded=True,
+    wireframe=True,
+    get_elevation="20",
+    get_fill_color="[137, 207, 240]",
+    get_line_color=[255, 255, 255],
+)
+
+INITIAL_VIEW_STATE = pdk.ViewState(latitude=geojson_dict['coordinates'][0][0][1], longitude=geojson_dict['coordinates'][0][0][0], zoom=9, max_zoom=16, pitch=45, bearing=0)
+
+# create the pydeck using the geojson layer
+r = pdk.Deck(layers=[ geojson_layer ], map_style=None, initial_view_state=INITIAL_VIEW_STATE)
+
+# display the pydeck
+st.pydeck_chart(r)
