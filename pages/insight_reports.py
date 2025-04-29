f1882163a68422882663bef6c2985c6f682efa6b
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-04-29 11:55:35 -0400

    Update insight_reports.py

--- pages/insight_reports.py
+++ pages/insight_reports.py
@@ -16,7 +16,6 @@ st.caption('Reports are maintained by [Disciplytics, LLC](https://www.disciplyti
 conn = st.connection("snowflake")
 
 single_tab, compare_tab = st.tabs(['Single Area', 'Compare Two Areas'])
-st.caption('Single Area is helpful when drilling down to one area. Compare Areas is helpful when looking into how a location compares to another.')
 
 with single_tab:
     # get geographical rel levels
@@ -32,40 +31,43 @@ with single_tab:
         geo_name_options = conn.query(f"SELECT DISTINCT GEO_NAME FROM  DISCIPLYTICS_APP.COMMUNITY_DATA.ACS_5YR_DATA WHERE LEVEL = '{geo_sel}' AND RELATED_GEO_NAME = '{geo_rel_sel}' ORDER BY GEO_NAME ASC;", ttl=0, show_spinner = False)
         geo_name_sel = st.selectbox(f"{geo_sel} Selection", geo_name_options['GEO_NAME'])
 
-    # connect to snowflake
-    @st.cache_data(show_spinner=f"Generating community analysis for {geo_name_sel}, {geo_rel_sel}.")
-    def load_acs_data_single(level, state, geo):
-        sql = f"SELECT * FROM DISCIPLYTICS_APP.COMMUNITY_DATA.ACS_5YR_DATA WHERE LEVEL = '{level}' AND RELATED_GEO_NAME = '{state}' AND GEO_NAME = '{geo}';"
-        return conn.query(sql, ttl=0, show_spinner = False)
-    # load the data
-    if geo_sel and geo_rel_sel:
-        acs_df = load_acs_data_single(geo_sel, geo_rel_sel, geo_name_sel)
-
-    # convert to dictionary
-    geojson_dict = loads(acs_df.head(1)['GEOJSON_VALUES'][0])
-        
-    # create the GeoJson layer
-    geojson_layer = pdk.Layer(
-            "GeoJsonLayer",
-            geojson_dict,
-            opacity=0.3,
-            stroked=False,
-            filled=True,
-            extruded=True,
-            wireframe=True,
-            get_elevation="20",
-            get_fill_color="[137, 207, 240]",
-            get_line_color=[255, 255, 255],
-        )
-        
-    INITIAL_VIEW_STATE = pdk.ViewState(latitude=geojson_dict['coordinates'][0][0][1], longitude=geojson_dict['coordinates'][0][0][0], zoom=9, max_zoom=16, pitch=45, bearing=0)
-        
-    # create the pydeck using the geojson layer
-    r = pdk.Deck(layers=[ geojson_layer ], map_style=None, initial_view_state=INITIAL_VIEW_STATE)
-    # display the pydeck
-    st.pydeck_chart(r)
+    if geo_name_sel and geo_rel_sel:
+        # connect to snowflake
+        @st.cache_data(show_spinner=f"Generating community analysis for {geo_name_sel}, {geo_rel_sel}.")
+        def load_acs_data_single(level, state, geo):
+            sql = f"SELECT * FROM DISCIPLYTICS_APP.COMMUNITY_DATA.ACS_5YR_DATA WHERE LEVEL = '{level}' AND RELATED_GEO_NAME = '{state}' AND GEO_NAME = '{geo}';"
+            return conn.query(sql, ttl=0, show_spinner = False)
+        # load the data
+        if geo_sel and geo_rel_sel:
+            acs_df = load_acs_data_single(geo_sel, geo_rel_sel, geo_name_sel)
     
-    st.dataframe(acs_df)
+        # convert to dictionary
+        geojson_dict = loads(acs_df.head(1)['GEOJSON_VALUES'][0])
+            
+        # create the GeoJson layer
+        geojson_layer = pdk.Layer(
+                "GeoJsonLayer",
+                geojson_dict,
+                opacity=0.3,
+                stroked=False,
+                filled=True,
+                extruded=True,
+                wireframe=True,
+                get_elevation="20",
+                get_fill_color="[137, 207, 240]",
+                get_line_color=[255, 255, 255],
+            )
+            
+        INITIAL_VIEW_STATE = pdk.ViewState(latitude=geojson_dict['coordinates'][0][0][1], longitude=geojson_dict['coordinates'][0][0][0], zoom=9, max_zoom=16, pitch=45, bearing=0)
+            
+        # create the pydeck using the geojson layer
+        r = pdk.Deck(layers=[ geojson_layer ], map_style=None, initial_view_state=INITIAL_VIEW_STATE)
+        # display the pydeck
+        st.pydeck_chart(r)
+        
+        st.dataframe(acs_df)
+    else:
+        st.write('Make the above selections.')
 
 
 with compare_tab:
