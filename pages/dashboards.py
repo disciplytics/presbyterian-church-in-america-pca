0689072a7f1200966c4953e2e62b91590649052b
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-04-29 10:33:54 -0400

    Update dashboards.py

--- pages/dashboards.py
+++ pages/dashboards.py
@@ -13,10 +13,6 @@ st.set_page_config(
     page_icon = 'https://jimdo-storage.freetls.fastly.net/image/446612637/7c401e7a-6b6d-4ec8-84a5-4ab2cae82c9e.png?quality=80,90&auto=webp&disable=upscale&width=1024&height=576&trim=0,0,0,0',
 )
 
-# LOGO
-st.image("https://media.licdn.com/dms/image/v2/D4E16AQGCrog5mV8nBQ/profile-displaybackgroundimage-shrink_350_1400/B4EZUAA8ZzHgAY-/0/1739462002589?e=1744848000&v=beta&t=miQyzZN82YjcYs9B_Mc-UVhaKt01dqVnPE56CnaVPbw",
-        width = 250)
-
 # title
 st.title('PCA Dashboards', help = 'This page offers visualizations of the PCA Statistics for improved reporting')
 st.caption('Reports are maintained by [Disciplytics, LLC](https://www.disciplytics.com/)')
