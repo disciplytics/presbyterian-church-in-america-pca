08acf21bf8fb6c017958febd9ef8c917c757816c
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-04-29 10:34:05 -0400

    Update home.py

--- pages/home.py
+++ pages/home.py
@@ -6,10 +6,6 @@ st.set_page_config(
     page_icon = 'https://jimdo-storage.freetls.fastly.net/image/446612637/7c401e7a-6b6d-4ec8-84a5-4ab2cae82c9e.png?quality=80,90&auto=webp&disable=upscale&width=1024&height=576&trim=0,0,0,0',
 )
 
-# LOGO
-st.image("https://media.licdn.com/dms/image/v2/D4E16AQGCrog5mV8nBQ/profile-displaybackgroundimage-shrink_350_1400/B4EZUAA8ZzHgAY-/0/1739462002589?e=1744848000&v=beta&t=miQyzZN82YjcYs9B_Mc-UVhaKt01dqVnPE56CnaVPbw",
-        width = 250)
-
 # title
 st.title('PCA Statistics Analysis :church:', help = 'All data is from [The PCA](https://presbyteryportal.pcanet.org/Report/StatsReport)')
 st.caption('App and analysis is maintained by [Disciplytics, LLC](https://www.disciplytics.com/)')
