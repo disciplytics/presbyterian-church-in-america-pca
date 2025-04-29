f56ac021d43e0e20e4034fd28b9ab6544be9d010
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-04-29 10:33:41 -0400

    Update insight_reports.py

--- pages/insight_reports.py
+++ pages/insight_reports.py
@@ -8,10 +8,6 @@ st.set_page_config(
     page_icon = 'https://jimdo-storage.freetls.fastly.net/image/446612637/7c401e7a-6b6d-4ec8-84a5-4ab2cae82c9e.png?quality=80,90&auto=webp&disable=upscale&width=1024&height=576&trim=0,0,0,0',
 )
 
-# LOGO
-st.image("https://media.licdn.com/dms/image/v2/D4E16AQGCrog5mV8nBQ/profile-displaybackgroundimage-shrink_350_1400/B4EZUAA8ZzHgAY-/0/1739462002589?e=1744848000&v=beta&t=miQyzZN82YjcYs9B_Mc-UVhaKt01dqVnPE56CnaVPbw",
-        width = 250)
-
 # title
 st.title('Community Insight Reports', help='This page offers community insights for each church. Great for ministry identification and evaluation.')
 st.caption('Reports are maintained by [Disciplytics, LLC](https://www.disciplytics.com/) and community data is generated from the [American Community Survey](https://www.census.gov/programs-surveys/acs/about.html)')
