c7925ed1909238dfd6945e724e2b4450a138fefe
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-04-30 19:01:02 -0400

    Update insight_reports.py

--- pages/insight_reports.py
+++ pages/insight_reports.py
@@ -191,7 +191,7 @@ with compare_tab:
             data = data[data['VARIABLE'].isin(variables)]
             
             data['Value'] = to_numeric(data['VALUE'])
-            data = data.rename(columns={'VARIABLE_NAME': 'Variable', 'GEO_NAME': 'Area'})
+            data = data.rename(columns={'SERIES_LEVEL_1': 'Variable', 'GEO_NAME': 'Area'})
             
             data = data.pivot_table('Value', ['Area', 'Variable'], 'MEASUREMENT_TYPE').reset_index()
 
