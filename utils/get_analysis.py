1512b22775b94cf9d83f0bdacf1742c208187496
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-04-30 19:33:23 -0400

    Update get_analysis.py

--- utils/get_analysis.py
+++ utils/get_analysis.py
@@ -30,16 +30,16 @@ def analysis_layout(data):
       
             st.altair_chart(alt.layer(bar, error, data=data).facet(column='Area:N'))
             
-    st.markdown('### Income Reports')
-    st.markdown('\n\n')
-    st.markdown('##### Income Inequality: Gini Index')
-    st.caption('Click here to learn more about this metric: [Gini Index](https://www.census.gov/topics/income-poverty/income-inequality/about/metrics/gini-index.html)')
+      st.markdown('### Income Reports')
+      st.markdown('\n\n')
+      st.markdown('##### Income Inequality: Gini Index')
+      st.caption('Click here to learn more about this metric: [Gini Index](https://www.census.gov/topics/income-poverty/income-inequality/about/metrics/gini-index.html)')
         
-    get_analysis(data, ['B19083_001M_5YR', 'B19083_001E_5YR'])
+      get_analysis(data, ['B19083_001M_5YR', 'B19083_001E_5YR'])
 
-    st.markdown('\n\n')
-    st.markdown('##### Household Income')
+      st.markdown('\n\n')
+      st.markdown('##### Household Income')
         
-    get_analysis(data, ['B19202_001E_5YR_2023','B19202_001M_5YR_2023','B19013_001E_5YR_2023','B19013_001M_5YR_2023','B19113_001E_5YR_2023','B19113_001M_5YR_2023'])
+      get_analysis(data, ['B19202_001E_5YR_2023','B19202_001M_5YR_2023','B19013_001E_5YR_2023','B19013_001M_5YR_2023','B19113_001E_5YR_2023','B19113_001M_5YR_2023'])
         
-    st.dataframe(data)
+      st.dataframe(data)
