31821c78f30a029be01f404999ebb4d64239980f
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-04-30 19:05:31 -0400

    Update insight_reports.py

--- pages/insight_reports.py
+++ pages/insight_reports.py
@@ -214,7 +214,7 @@ with compare_tab:
 
             
             
-            st.altair_chart(alt.layer(bar, error, data=data).facet(facet=alt.Facet('Area:N')))
+            st.altair_chart(alt.layer(bar, error, data=data).facet(column='Area:N'))
 
 
         st.markdown('### Income Reports')
