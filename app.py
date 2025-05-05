ce90da605ae37aabe647afce5ae478faf52d5f34
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-05-05 14:47:23 -0400

    Update app.py

--- app.py
+++ app.py
@@ -14,18 +14,11 @@ dashreport_page = st.Page(
   default=False
 )
 
-community_insight_page = st.Page(
-  'pages/insight_reports.py', 
-  title = 'Community Insight Reports', 
-  icon=":material/communities:", 
-  default=False
-)
-
 # navigation 
 pg = st.navigation(
         {
             "": [home_page],
-            "Data Reports": [dashreport_page, community_insight_page],
+            "Data Reports": [dashreport_page],
           
         }
     )
