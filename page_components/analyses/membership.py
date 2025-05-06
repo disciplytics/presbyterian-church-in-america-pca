7ee615cce7bc0a22272c1b2c0243f41e69e03625
disciplytics <122502050+disciplytics@users.noreply.github.com>
2025-05-06 18:40:31 -0400

    Update membership.py

--- page_components/analyses/membership.py
+++ page_components/analyses/membership.py
@@ -2,15 +2,15 @@ def membership(df):
   import streamlit as st
   import pandas as pd
 
-  df['Total Members'] = df['COMM'] + df['NONCOMM']
-  df['Total Additions'] = df['PROFCHILD'] + df['PROFADULT'] + df['TRANSLETTER'] + df['REAFFIRMATION'] + df['RESTORED']
-  df['Total Losses'] = df['DEATH'] + df['TRANS'] + df['REMOVEDFROMROLL'] + df['DISCIPLINE']
+  df['Total Members'] = df['COMM'] + df['NON_COMM']
+  df['Total Additions'] = df['PROF_CHILD'] + df['PROF_ADULT'] + df['TRANS_LETTER'] + df['REAFFIRMATION'] + df['RESTORED']
+  df['Total Losses'] = df['DEATH'] + df['TRANS'] + df['REMOVED_FROM_ROLL'] + df['DISCIPLINE']
   
   df = df.rename(columns = {
-          'YEAR': 'Year',
-          'COMM': 'Comm', 'NONCOMM': 'NonComm', 
-          'PROFCHILD': 'ProfChild', 'PROFADULT': 'ProfAdult', 'TRANSLETTER': 'TransLetter', 'REAFFIRMATION': 'Reaffirmation', 'RESTORED': 'Restored',
-          'DEATH': 'Death', 'TRANS': 'Trans', 'REMOVEDFROMROLL': 'RemovedFromRoll', 'DISCIPLINE': 'Discipline',
+          'STAT_YEAR': 'Year',
+          'COMM': 'Comm', 'NON_COMM': 'NonComm', 
+          'PROF_CHILD': 'ProfChild', 'PROF_ADULT': 'ProfAdult', 'TRANS_LETTER': 'TransLetter', 'REAFFIRMATION': 'Reaffirmation', 'RESTORED': 'Restored',
+          'DEATH': 'Death', 'TRANS': 'Trans', 'REMOVED_FROM_ROLL': 'RemovedFromRoll', 'DISCIPLINE': 'Discipline',
   }
                 )
 
